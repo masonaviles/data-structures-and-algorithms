2721f98aff976358bce0490c8e78f30103b7498e
Mason Aviles <mce.aviles@gmail.com>
2021-05-18 20:39:41 -0700

    finish

--- python/code_challenges/array-shift.py
+++ python/code_challenges/array-shift.py
@@ -1,21 +1,22 @@
 def insert(arr, num):
   counter = 0
   is_true = True
+  new = []
   print(len(arr)/2)
   while is_true:
-    # if counter < half length & append
-    if counter < (len(arr)/2):
-      print(arr[counter])
-    # elif counter = half & append new number
-    elif counter == (len(arr)/2):
-      print(num)
-    # elif counter = length & send a false
-    elif counter <= len(arr):
-      print(arr[counter-1])      
+    if counter < round(len(arr)/2):
+        new.append(arr[counter])
+    elif counter == round(len(arr)/2):
+      new.append(num)
+    elif counter <= round(len(arr)):
+      new.append(arr[counter - 1])     
     else:
       is_true = False
       print("send false")
+      print(new)
+      return new
 
     counter +=1
 
 insert([1, 2, 3, 4, 5, 6], 8)
+insert([1, 2, 3, 4, 5, 6, 7], 8)
