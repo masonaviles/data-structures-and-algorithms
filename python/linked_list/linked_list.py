9a75274c4189f0a5aa6a5ed90afbfafdfd2be641
Mason Aviles <mce.aviles@gmail.com>
2021-05-23 20:45:13 -0700

    linked lists

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,33 @@
 class LinkedList:
-    """
-    Put docstring here
-    """
-
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
+        
+    # return the whole list in the linked list
+    def to_list(self):
+        values = []
+        current = self.head
+        while current:
+            values.append(current.value)
+            current = current.next
+        return values
 
-    def some_method(self):
-        # method body here
-        pass
+    # traverse list and check to see if value is in a LinkedList
+    def includes(self, value):
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
+    
+    # insert at the beginning
+    def insert(self, value):
+        # old_head = self.head
+        # node = Node(value, old_head)
+        self.head = Node(value, self.head)
+        # node.next = old_head
+
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
