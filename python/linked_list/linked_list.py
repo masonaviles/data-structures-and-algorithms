8df22ac118339e2a0c5b333becda8654978e9497
Mason Aviles <mce.aviles@gmail.com>
2021-05-24 22:26:12 -0700

    linked lists

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,63 @@
 class LinkedList:
-    """
-    Put docstring here
-    """
-
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, value):
+        self.head = Node(value, self.head)
+
+    def includes(self, search_value):
+        current = self.head
+        while current:
+            if current.value == search_value:
+                return True
+            current = current.next
+        return False
+
+    def __str__(self):
+        string = ''
+        current = self.head
+        while current:
+            string += f'{current.value} --> '
+            current = current.next
+        string += 'None'
+        return string
+
+    def append(self, value):
+      current = self.head
+      while current:
+        if current.next == None:
+          current.next = Node(value)
+          break
+        current = current.next
+
+    def insertBefore(self, value, new_value):
+      current = self.head
+      previous = None
+      while current:
+        if self.head.value == value:
+          self.insert(new_value)
+          break
+        if current.value == value:
+          previous.next = Node(new_value, current)
+          break
+        previous = current
+        current = current.next
+
+    def insertAfter(self, value, new_value):
+      current = self.head
+      insert_before_this = current.next
+      while current:
+        if self.head.value == value:
+          self.insert_before(insert_before_this.value, new_value)
+          break
+        if current.value == value:
+          current.next = Node(new_value, insert_before_this)
+          break
+        current = current.next
+        insert_before_this = current.next
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
