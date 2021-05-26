d4d11b2e9db764fed7c4a3f16a5c47e52b9a3395
Mason Aviles <mce.aviles@gmail.com>
2021-05-25 21:29:58 -0700

    adding code of linked_list, Find k’th node from the end of a linked list

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -7,6 +7,20 @@ class LinkedList:
         # initialization here
         pass
 
-    def some_method(self):
-        # method body here
-        pass
+    # Input -> takes a number, k
+    # Output -> the node’s value that is [k] from the end of the linked list
+
+    def nthFromLast(self, k): # 5
+        current = self.head
+        length = 0
+
+        while current:
+            current = current.next
+            length = length + 1
+         
+        if length >= k:
+            current = self.head
+            for i in range(length - k):
+                current = current.next
+
+        return current.data
