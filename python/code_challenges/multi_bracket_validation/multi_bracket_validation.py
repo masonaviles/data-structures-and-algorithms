d6a7411002288e8e41a74f84565cf699e8b6d601
Mason Aviles <mce.aviles@gmail.com>
2021-06-06 21:59:32 -0700

    work on brackets

--- python/code_challenges/multi_bracket_validation/multi_bracket_validation.py
+++ python/code_challenges/multi_bracket_validation/multi_bracket_validation.py
@@ -2,14 +2,14 @@ def multiBracketValidation(input):
     open_brackets = [ "[", "{", "(" ]
     close_brackets = [ "]", "}", ")" ]
     stack = []
-    for letter in input:
-        if letter in open_brackets:
-            stack.append(letter)
-        elif letter in close_brackets:
+    for char in input:
+        if char in open_brackets:
+            stack.append(char)
+        elif char in close_brackets:
             # if we encounter a closed bracket but there was never an open bracket in the stack to begin with, then False
             if len(stack) <= 0:
                 return False
             # if what we popped from stack (which should be all opens) doesn't match the close bracket we just found, then False
-            if open_brackets.index(stack.pop()) != close_brackets.index(letter):
+            if open_brackets.index(stack.pop()) != close_brackets.index(char):
                 return False
     return True
