c1a8fefc90a46a961325efdb75d6be7979b02bc6
Mason Aviles <mce.aviles@gmail.com>
2021-06-08 22:08:37 -0700

    breadth first

---
+++ python/code_challenges/breadth_first_binary_tree/breadth_first.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def breadth_first(root):
+    # Base Case
+    if root is None:
+        return
+    # Create an empty queue
+    q = deque()
+    bf_list = []
+    # Enqueue Root
+    q.append(root)
+    # while there are things in the queue loop
+    while(len(q) > 0):
+        # add front of queue to list and
+        # remove it from queue
+        bf_list.append(q[0].data)
+        node = q.pop(0)
+        # Enqueue left child
+        if node.left is not None:
+            q.append(node.left)
+        # Enqueue right child
+        if node.right is not None:
+            q.append(node.right)
+    return bf_list
---
+++ python/tests/test_breadth_first.py
@@ -0,0 +1,5 @@
+from code_challenges.breadth_first_binary_tree.breadth_first import breadth_first
+
+
+def test_import():
+    assert breadth_first
