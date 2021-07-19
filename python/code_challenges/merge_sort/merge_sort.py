cc7d5878d94b540da93006a39d9c478e2982efb2
Mason Aviles <mce.aviles@gmail.com>
2021-07-19 00:19:22 -0700

    merge sort

--- python/code_challenges/merge_sort/merge_sort.py
+++ python/code_challenges/merge_sort/merge_sort.py
@@ -17,17 +17,35 @@
 #     DECLARE j <-- 0
 #     DECLARE k <-- 0
 
-def Mergesort(arr):
-    n = len(arr)
-    if n > 1:
-        mid = n//2
-        left = arr[:mid]
-        right = arr[mid:]
-        Mergesort(left)
-        Mergesort(right)
-        Merge(left, right, arr)
+def merge_sort(list):
+    key = len(list)
 
-def Merge(left, right, arr):
+    if key > 1:
+        mid = key // 2
+        left = list[0:mid]
+        right = list[mid:key]
+        merge_sort(left)
+        merge_sort(right)
+        return merge(left, right, list)
+
+def merge(left, right, list):
+
     i = 0
     j = 0
-    k = 0
+    x = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            list[x] = left[i]
+            i = i + 1
+
+        else:
+            list[x] = right[j]
+            j = j + 1
+
+        x = x + 1
+
+    if i == len(left):
+        list[x:] = right[j:] 
+        list[x:] = left[i:]
+    return list
